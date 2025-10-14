303bcb1c69d078aa41e815057dcb2f33969a78a8
daridev <darideveloper@gmail.com>
2025-10-14 14:27:01 -0600

    fix (roulette/admin): update main setup

--- roulette/admin.py
+++ roulette/admin.py
@@ -1,29 +1,71 @@
 from django.contrib import admin
 from roulette import models
 
+
 @admin.register(models.Roulette)
 class RouletteAdmin(admin.ModelAdmin):
-    list_display = ("name", "slug", "logo", "subtitle", "bottom_text", "bg_image", "current_spins", "wrong_icon", "message_error_no_spin", "message_lose_lose", "message_win", "color_spin_1", "color_spin_2", "color_spin_3", "color_spin_4", "created_at", "updated_at")
+    list_display = (
+        "name",
+        "subtitle",
+        "current_spins",
+        "created_at",
+        "updated_at",
+    )
     list_filter = ("created_at", "updated_at")
-    search_fields = ("name", "slug", "logo", "subtitle", "bottom_text", "bg_image", "current_spins", "wrong_icon", "message_error_no_spin", "message_lose_lose", "message_win", "color_spin_1", "color_spin_2", "color_spin_3", "color_spin_4")
-    readonly_fields = ("created_at", "updated_at")
+    search_fields = (
+        "name",
+        "subtitle",
+        "bottom_text",
+        "message_error_no_spin",
+        "message_lose_lose",
+        "message_win",
+    )
+    readonly_fields = ("slug", "created_at", "updated_at")
+
 
 @admin.register(models.Award)
 class AwardAdmin(admin.ModelAdmin):
-    list_display = ("name", "description", "min_spins", "image", "active", "created_at", "updated_at")
-    list_filter = ("created_at", "updated_at")
-    search_fields = ("name", "description", "min_spins", "image", "active")
+    list_display = (
+        "name",
+        "roulette",
+        "description",
+        "active",
+        "created_at",
+        "updated_at",
+    )
+    list_filter = ("roulette", "active", "created_at", "updated_at")
+    search_fields = ("name", "description")
+
 
 @admin.register(models.Participant)
 class ParticipantAdmin(admin.ModelAdmin):
-    list_display = ("name", "email", "last_spin", "last_extra_spin", "created_at", "updated_at")
-    list_filter = ("created_at", "updated_at")
-    search_fields = ("name", "email", "last_spin", "last_extra_spin")
+    list_display = (
+        "name",
+        "email",
+        "last_spin",
+        "last_extra_spin",
+        "created_at",
+        "updated_at",
+    )
+    list_filter = (
+        "last_spin",
+        "last_extra_spin",
+        "created_at",
+        "updated_at",
+    )
+    search_fields = ("name", "email")
     readonly_fields = ("created_at", "updated_at")
+
 
 @admin.register(models.ParticipantAward)
 class ParticipantAwardAdmin(admin.ModelAdmin):
     list_display = ("participant", "award", "created_at", "updated_at")
-    list_filter = ("created_at", "updated_at")
-    search_fields = ("participant", "award")
+    list_filter = ("participant", "award__roulette", "created_at", "updated_at")
+    search_fields = (
+        "participant__name",
+        "participant__email",
+        "award__name",
+        "award__roulette__name",
+
+    )
     readonly_fields = ("created_at", "updated_at")
