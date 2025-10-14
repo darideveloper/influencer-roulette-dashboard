ef56f55394d12992f3962bf5473a546ff23230a2
daridev <darideveloper@gmail.com>
2025-10-14 14:11:10 -0600

    chore (roulette/models): add verbose name & help text in spanish

--- roulette/models.py
+++ roulette/models.py
@@ -2,38 +2,63 @@ from django.db import models
 
 
 class Roulette(models.Model):
-    name = models.CharField(max_length=255)
+    # Main data
+    name = models.CharField(max_length=255, verbose_name="Nombre")
     slug = models.SlugField(
-        max_length=255, unique=True, help_text="A unique slug for the roulette URL."
+        max_length=255,
+        unique=True,
+        verbose_name="Slug",
+        help_text="url autogenerada para la ruleta.",
     )
-    logo = models.ImageField(upload_to="roulette/logos/")
-    subtitle = models.CharField(max_length=255, default="", blank=True)
-    bottom_text = models.TextField(default="", blank=True)
-    bg_image = models.ImageField(upload_to="roulette/backgrounds/")
+    logo = models.ImageField(upload_to="roulette/logos/", verbose_name="Logo")
+    subtitle = models.CharField(
+        max_length=255, default="", blank=True, verbose_name="Subtítulo"
+    )
+    bottom_text = models.TextField(
+        default="", blank=True, verbose_name="Texto inferior"
+    )
+    bg_image = models.ImageField(
+        upload_to="roulette/backgrounds/", verbose_name="Imagen de fondo"
+    )
     current_spins = models.IntegerField(
-        default=0, help_text="Tracks the total number of spins for this roulette."
+        default=0,
+        verbose_name="Total de giros",
+        help_text="Total de giros realizados en la ruleta. No editar manualmente.",
     )
-    wrong_icon = models.ImageField(upload_to="roulette/icons/")
-    message_error_no_spin = models.CharField(max_length=255)
-    message_lose_lose = models.CharField(max_length=255)
-    message_win = models.CharField(max_length=255)
+    wrong_icon = models.ImageField(
+        upload_to="roulette/icons/", verbose_name="Icono de error (sin premio)"
+    )
+    message_error_no_spin = models.CharField(
+        max_length=255, verbose_name="Mensaje de error (sin mas giros)"
+    )
+    message_lose_lose = models.CharField(
+        max_length=255, verbose_name="Mensaje de error (perdió)"
+    )
+    message_win = models.CharField(
+        max_length=255, verbose_name="Mensaje de felicitaciones (ganó)"
+    )
 
-    # accepts hex color code, e.g., #FFFFFF
+    # Colors
     color_spin_1 = models.CharField(
-        max_length=7, help_text="Hex color code, e.g., #FFFFFF"
+        max_length=7, verbose_name="Color 1", help_text="Hex color code, e.g., #FFFFFF"
     )
     color_spin_2 = models.CharField(
-        max_length=7, help_text="Hex color code, e.g., #FFFFFF"
+        max_length=7, verbose_name="Color 2", help_text="Hex color code, e.g., #FFFFFF"
     )
     color_spin_3 = models.CharField(
-        max_length=7, help_text="Hex color code, e.g., #FFFFFF"
+        max_length=7, verbose_name="Color 3", help_text="Hex color code, e.g., #FFFFFF"
     )
     color_spin_4 = models.CharField(
-        max_length=7, help_text="Hex color code, e.g., #FFFFFF"
+        max_length=7, verbose_name="Color 4", help_text="Hex color code, e.g., #FFFFFF"
     )
 
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    # dates
+    created_at = models.DateTimeField(
+        auto_now_add=True, verbose_name="Fecha de creación"
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True, verbose_name="Fecha de actualización"
+    )
 
     class Meta:
         verbose_name = "Ruleta"
@@ -45,19 +70,29 @@ class Roulette(models.Model):
 
 class Award(models.Model):
     roulette = models.ForeignKey(
-        Roulette, on_delete=models.CASCADE, related_name="awards"
+        Roulette, on_delete=models.CASCADE, related_name="awards", verbose_name="Ruleta"
     )
-    name = models.CharField(max_length=255)
-    description = models.TextField(default="", blank=True)
+    name = models.CharField(max_length=255, verbose_name="Nombre")
+    description = models.TextField(default="", blank=True, verbose_name="Descripción")
     min_spins = models.IntegerField(
-        help_text="Minimum spins required to be eligible for this award."
+        verbose_name="Mínimo de giros",
+        help_text="Mínimo de giros requeridos para ser elegible para este premio.",
     )
-    image = models.ImageField(upload_to="awards/")
-    active = models.BooleanField(default=True)
+    image = models.ImageField(upload_to="awards/", verbose_name="Imagen del premio")
+    active = models.BooleanField(
+        default=True,
+        verbose_name="Activo",
+        help_text="Si está activo, el premio se mostrará en la ruleta.",
+    )
 
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    
+    # dates
+    created_at = models.DateTimeField(
+        auto_now_add=True, verbose_name="Fecha de creación"
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True, verbose_name="Fecha de actualización"
+    )
+
     class Meta:
         verbose_name = "Premio"
         verbose_name_plural = "Premios"
@@ -68,23 +103,35 @@ class Award(models.Model):
 
 class Participant(models.Model):
     roulette = models.ForeignKey(
-        Roulette, on_delete=models.CASCADE, related_name="participants"
+        Roulette,
+        on_delete=models.CASCADE,
+        related_name="participants",
+        verbose_name="Ruleta",
     )
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, verbose_name="Nombre")
     email = models.EmailField(
-        unique=True, help_text="Each participant must have a unique email."
+        unique=True,
+        verbose_name="Email",
     )
-    last_spin = models.DateTimeField(null=True, blank=True)
-    last_extra_spin = models.DateTimeField(null=True, blank=True)
+    last_spin = models.DateTimeField(null=True, blank=True, verbose_name="Último giro")
+    last_extra_spin = models.DateTimeField(
+        null=True, blank=True, verbose_name="Último giro extra"
+    )
 
     # Many-to-Many relationship
     awards = models.ManyToManyField(
-        Award, through="ParticipantAward", related_name="winners"
+        Award,
+        through="ParticipantAward",
+        related_name="winners",
+        verbose_name="Premios",
     )
-
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
     
+    # dates
+    created_at = models.DateTimeField(
+        auto_now_add=True, verbose_name="Fecha de creación"
+    )
+    updated_at = models.DateTimeField(auto_now=True)
+
     class Meta:
         verbose_name = "Participante"
         verbose_name_plural = "Participantes"
@@ -94,12 +141,18 @@ class Participant(models.Model):
 
 
 class ParticipantAward(models.Model):
-    participant = models.ForeignKey(Participant, on_delete=models.CASCADE)
-    award = models.ForeignKey(Award, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(
-        auto_now_add=True, help_text="Timestamp of when the award was won."
+    participant = models.ForeignKey(
+        Participant, on_delete=models.CASCADE, verbose_name="Participante"
     )
-    updated_at = models.DateTimeField(auto_now=True)
+    award = models.ForeignKey(Award, on_delete=models.CASCADE, verbose_name="Premio")
+    
+    # dates
+    created_at = models.DateTimeField(
+        auto_now_add=True, help_text="Fecha y hora de cuando el premio fue ganado."
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True, verbose_name="Fecha de actualización"
+    )
 
     class Meta:
         verbose_name = "Premio de Participante"
